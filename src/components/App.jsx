b3eaf954a3ed1cd3e0df3e2b7cbb095feda21e86
vokur13 <vokur13@gmail.com>
2022-09-23 14:37:43 +0300

    not resolved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import './App.css';
+import Table1 from './Table/Table1';
+
+var data = [
+  { id: 1, name: 'Gob', value: '2' },
+  { id: 2, name: 'Buster', value: '5' },
+  { id: 3, name: 'George Michael', value: '4' },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="Table-header">Basic Table</p>
+        <Table1 data={data} />
+      </div>
+    );
+  }
+}
+
+export default App;
